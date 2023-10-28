944b2cac1caca9137269319c8fbb2c9dca17f757
waqas <waqas.se2020@gmail.com>
2023-10-28 18:45:08 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,158 @@
+
+// // // // // // // // // let userName = +prompt("Enter your name");
+
+// // // // // // // // // let physicsMarks = +prompt("Enter your Physics Marks");
+
+// // // // // // // // // let englishMarks = +prompt("Enter your English Marks");
+
+// // // // // // // // // let mathMarks= +prompt("Enter your Math Marks");
+
+// // // // // // // // // let urduMarks= +prompt("Enter your Urdu Marks");
+
+// // // // // // // // // let sciencMarks= +prompt("Enter your Science Marks");
+
+// // // // // // // // // let totalMarks = 500;
+
+
+
+// // // // // // // // // let percentage = obtain / 
+
+
+
+
+
+// // // // // // // // // const percentage = +prompt("Enter your percentage");
+
+// // // // // // // // // if (percentage >= 80) {
+
+// // // // // // // // //     console.log("Grade A+");
+
+// // // // // // // // // } else if (percentage < 80 && percentage >= 70 ){
+
+// // // // // // // // //     console.log("grade A");
+
+// // // // // // // // // } else if ( percentage < 70 && percentage >= 60){
+
+// // // // // // // // //     console.log(" Grade B");
+
+// // // // // // // // // } else {
+// // // // // // // // //     console.log("CHUTTI KR ");
+// // // // // // // // // }
+
+// // // // // // // // let wicket = +prompt("enter remaining wicket")
+
+// // // // // // // // if (wicket >= 4) {
+// // // // // // // //     console.log("Pakistan Won the Match");    
+// // // // // // // // } else if ( wicket >= 6 ){
+// // // // // // // //     console.log("Match has been drawn");
+// // // // // // // // } else {
+// // // // // // // //     console.log("Afghanistan won the match");
+// // // // // // // // }
+
+
+// // // // // // // function greetUser() {
+    
+// // // // // // //     let userInput = +prompt("Enter your name");
+// // // // // // //     console.log(userInput);
+
+
+// // // // // // // }
+
+// // // // // // // greetUser();
+
+
+
+// // // // // // // function FunctionName(numb1, numb2) {
+// // // // // // //     console.log(numb1 + numb2);
+// // // // // // // }
+
+// // // // // // // FunctionName(78, 89)
+
+
+// // // // // // function IntroOfPerson(name, age, salary ) {
+    
+// // // // // //     console.log(` Hii, My name is ${name}, adn i am ${age} year Old, and my salart is ${salary}. `);
+// // // // // // }
+
+// // // // // // IntroOfPerson("Waqas", 26, 20000)
+
+
+// // // function Newfunction (num1, num2, num3, sumOfNumber) {
+
+// // //      num1 = +prompt("Enter your age")
+// // //      num2 = +prompt("Enter your brother's age")
+// // //      num3 = +prompt("Enter your sister's age")
+// // //     sumOfNumber = num1 + num2 + num3;
+// // //      console.log( sumOfNumber );
+
+// // //      if (sumOfNumber > 10) {
+// // //         console.log("10 sy zyada numbers hein")
+        
+// // //      } else {
+// // //         console.log("10 sy kam hein ustad")
+// // //      }
+// // // // 
+
+
+// // // }
+
+// // // Newfunction()
+
+
+// // // // function HelloUser() {
+
+// // // //     console.log("Hello WOrld");
+// // // //     return " Hello User"
+// // // // }
+
+// // // // console.log(HelloUser());
+
+
+// // function sumofTwoNumbers(num1, num2, num3) {
+
+// //     num1 = +prompt("Enter Your age")
+// //     num2 = +prompt("Enter your brother's age")
+
+// //     num3 = num1 + num2
+
+// //     return num3
+
+    
+// // }
+
+// // console.log(sumofTwoNumbers());
+
+// function greetUser(num1, num2, num3) {
+
+//     num1 = +prompt("Enter Your age") ;
+//     num2 = +prompt("Enter your brother's age") ;    
+//     num3 =  num1 + num2
+
+//     return num3
+
+// }
+
+// //user se primtm mem naem lena eh phr do produc ke name lena phr pruce leni he, console me user ka name product ka name or price print krwani he 
+
+
+// function getUserData() {
+//     const name = prompt("Enter Your name");
+//     const p1 = prompt("Enter first product name");
+//     const p2 = prompt("Enter second product name");
+//     const price1 = +prompt("Enter first product price");
+//     const price2 = +prompt("Enter second product price");
+
+//     return {
+//         name: name,
+//         p1: p1,
+//         p2: p2,
+//         price1: price1,
+//         price2: price2
+//     };
+// }
+
+// const userData = getUserData();
+// const message = `Your name is ${userData.name}, first product name is ${userData.p1}, second product name is ${userData.p2}, and the price of the first product is ${userData.price1} and the price of the second product is ${userData.price2}`;
+// console.log(message);
+
+
